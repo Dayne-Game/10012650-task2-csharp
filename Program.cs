bc4af7104584fc799d3368e2222c9cbbf8f561a1
Dayne Game <daynegame024@gmail.com>
2017-08-14 11:57:18 +1200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _10012650_task2_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userInput = Console.ReadLine();
+            
+            var number = new string[10];
+            var count = 10;
+
+            for (var i = 0; i < count; i++)
+            {
+                Console.WriteLine("Type in a Number");
+                double number1 = double.Parse(Console.ReadLine());
+                double convertedNumber = Math.Round(number1 * 10, 2);
+
+                Console.WriteLine($"The average is {convertedNumber}");
+
+                
+
+            }
+        }
+    }
+}
